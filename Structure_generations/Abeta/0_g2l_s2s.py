c81d2b6d2c018343451afff57fb43c02adfad0c0
wang5115 <168797942+wang5115@users.noreply.github.com>
2025-04-30 01:42:52 -0400

    Add files via upload

---
+++ Structure_generations/Abeta/0_g2l_s2s.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+import os
+import shutil
+import numpy as np
+
+# Load the PDB file
+file_path = '/home/yanbin/Desktop/Projects/GolP/Full_data_code/4_18/Au_F_2/KLVFFAE.pdb'
+
+with open(file_path, 'r') as file:
+    pdb_lines = file.readlines()
+
+# Split frames using ENDMDL as separator
+frames = []
+current_frame = []
+
+for line in pdb_lines:
+    current_frame.append(line)
+    if line.startswith('ENDMDL'):
+        frames.append(current_frame)
+        current_frame = []
+
+# Ensure output directory exists
+output_dir = '/home/yanbin/Desktop/Projects/GolP/Full_data_code/4_18/Au_F_2/individual'
+os.makedirs(output_dir, exist_ok=True)
+
+# Save each frame into its own file
+for i, frame in enumerate(frames):
+    frame_file_path = os.path.join(output_dir, f'KLVFFAE_2_{i+1}.pdb')
+    with open(frame_file_path, 'w') as frame_file:
+        frame_file.write(''.join(frame))
+
+# Optional: Zip the folder for easier handling
+#shutil.make_archive('individual_frames', 'zip', output_dir)
+
+print(f'Split {len(frames)} frames into individual files in "{output_dir}/".')
+#print('A zip file "individual_frames.zip" has also been created.')
+
+#####################################
---
+++ Structure_generations/Abeta/1_GLY_removal.py
@@ -0,0 +1,10 @@
+input_pdb = "/home/yanbin/Apps/Str2Str/logs/inference/runs/Au_F_2/samples/all_delta/GKLVFFAEG_F_2.pdb"
+output_pdb = input_pdb.replace(".pdb", "_noGLY.pdb")
+
+with open(input_pdb, "r") as infile, open(output_pdb, "w") as outfile:
+    for line in infile:
+        if line.startswith(("ATOM", "HETATM")) and line[17:20].strip() == "GLY":
+            continue
+        outfile.write(line)
+
+print(f"Saved to: {output_pdb}")
