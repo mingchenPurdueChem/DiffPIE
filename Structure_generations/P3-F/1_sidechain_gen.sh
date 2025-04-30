aaea1a4ce3bbc5e35ac6b1e83f5a232a96f83588
wang5115 <168797942+wang5115@users.noreply.github.com>
2025-04-30 01:28:41 -0400

    Update 1_sidechain_gen.sh

--- Structure_generations/P3-F/1_sidechain_gen.sh
+++ Structure_generations/P3-F/1_sidechain_gen.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Create the output directory if it doesn't exist
-mkdir -p /home/yanbin/Desktop/Projects/organic_linker/cyclic_linker_2.0/data/biased/2/individual_frames_pdb_str2str_local_reset_AA
+mkdir -p data/individual_frames_pdb_str2str_local_reset_AA
 
 # Loop through all PDB files and run FASPR
-for pdb_file in /home/yanbin/Desktop/Projects/organic_linker/cyclic_linker_2.0/data/biased/2/individual_frames_pdb_str2str_local_reset/*.pdb; do
+for pdb_file in data/individual_frames_pdb_str2str_local_reset/*.pdb; do
     base_name=$(basename "$pdb_file")
-    output_name="/home/yanbin/Desktop/Projects/organic_linker/cyclic_linker_2.0/data/biased/2/individual_frames_pdb_str2str_local_reset_AA/${base_name%.pdb}_AA.pdb"
+    output_name="data/individual_frames_pdb_str2str_local_reset_AA/${base_name%.pdb}_AA.pdb"
     ./FASPR -i "$pdb_file" -o "$output_name"
 done
